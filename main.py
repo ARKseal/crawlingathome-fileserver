3b666bd0c1a2809cc5e3dc2ca4e8e0e68ad3b3f9
ARKseal <56448385+ARKseal@users.noreply.github.com>
2021-06-28 20:39:06 -0500

    Fix typo

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.config['BODY_TIMEOUT'] = 60 * 60 # 1 hour
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in app.config['UPLOAD_FOLDER']
+           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 @app.route('/upload', methods=['POST'])
 async def upload_file():
